08695574eb60098213aa95ac44df89f2a82f7c9d
Melker Ulander <melker.ulander@pm.me>
2025-09-26 11:38:16 +0200

    Fix issue with hyprshell's alt-tab

--- home/default.nix
+++ home/default.nix
@@ -21,31 +21,11 @@
     hyprshell = {
       enable = true;
       settings = {
+        version = 2;
         windows = {
-          scale = 8;
-          items_per_row = 5;
-          overview = {
-            key = "";
-            modifier = "super";
-            filter_by = [ ];
-            hide_filtered = false;
-            launcher = {
-              default_terminal = "ghostty";
-              launch_modifier = "alt";
-              width = 650;
-              max_items = 5;
-              show_when_empty = true;
-              plugins.applications = {
-                run_cache_weeks = 4;
-                show_execs = true;
-                show_actions_submenu = true;
-              };
-            };
-          };
           switch = {
-            modifier = "alt";
-            filter_by = [ ];
             switch_workspaces = false;
+            modifier = "alt";
           };
         };
       };
