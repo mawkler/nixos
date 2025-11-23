59f29d37ff7d8e551cf0b268a2dd353f7bee0851
Melker Ulander <melker.ulander@pm.me>
2025-11-23 23:20:13 +0100

    Add swayidle config with locking and suspending

--- home/default.nix
+++ home/default.nix
@@ -11,6 +11,36 @@
 
   nixpkgs = { inherit overlays; };
 
+  services.swayidle = let
+    minutes = 60;
+    suspend = "${pkgs.systemd}/bin/systemctl suspend";
+    lock = "${pkgs.systemd}/bin/loginctl lock-session";
+    display = status:
+      "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+  in {
+    enable = true;
+    timeouts = [
+      {
+        timeout = 10 * minutes;
+        command = lock;
+      }
+      {
+        timeout = 11 * minutes;
+        command = display "off";
+        resumeCommand = display "on";
+      }
+      {
+        timeout = 25 * minutes;
+        # Don't suspend if laptop charger is plugged in
+        command = "systemd-ac-power || ${suspend}";
+      }
+    ];
+    events = [{
+      event = "before-sleep";
+      command = lock;
+    }];
+  };
+
   programs = {
     zen-browser.enable = true;
 
