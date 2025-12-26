868c083f997e70efa5acf589e4128b4d6b752404
Melker Ulander <melker.ulander@pm.me>
2025-12-26 23:00:57 +0100

    Add some more fish binds

--- home/shell/fish/config.fish
+++ home/shell/fish/config.fish
@@ -7,6 +7,10 @@ end
 set -g fish_key_bindings fish_vi_key_bindings
 
 # Keybindings
-bind -M insert ctrl-p 'up-or-search'
-bind -M insert ctrl-n 'down-or-search'
-bind -M insert ctrl-f 'forward-char'
+bind -M insert ctrl-p up-or-search
+bind -M insert ctrl-n down-or-search
+bind -M insert ctrl-f forward-char
+bind -M insert alt-backspace backward-kill-path-component
+bind -M insert alt-shift-p fish_clipboard_paste
+bind -M insert alt-L 'commandline "ls -a"' execute
+bind -M insert alt-z zi repaint
