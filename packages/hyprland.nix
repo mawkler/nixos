365500e7c2769120dc6308e10b6d4ebb2ea21375
Melker Ulander <melker.ulander@pm.me>
2026-05-13 11:39:49 +0200

    Properly activate graphical-session.target services

--- packages/hyprland.nix
+++ packages/hyprland.nix
@@ -1,21 +1,29 @@
 { pkgs, inputs, ... }:
 {
+  imports = [ inputs.dank-material-shell.nixosModules.greeter ];
+
   programs.hyprland =
     let
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system};
     in
     {
       enable = true;
+      # Properly activate `graphical-session.target` related services (e.g. DMS and Vicinae)
+      withUWSM = true;
 
       # TODO: remove these two lines once the flake is removed in favour of nixpkgs
       package = package.hyprland;
       portalPackage = package.xdg-desktop-portal-hyprland;
     };
 
-  services.displayManager.sddm = {
+  # Display manager
+  programs.dank-material-shell.greeter = {
     enable = true;
-    wayland.enable = true;
+    compositor.name = "hyprland";
   };
+
+  # Optional, hint electron apps to use wayland (from Hyprland's docs)
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   environment.systemPackages = with pkgs; [
     brightnessctl
