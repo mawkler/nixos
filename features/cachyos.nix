bc2931ae27baa82b9f75ef8c12370fab75999f89
Melker Ulander <melker.ulander@pm.me>
2026-07-25 19:43:32 +0200

    cachyos: remove garnix cache since it's deprecated

--- features/cachyos.nix
+++ features/cachyos.nix
@@ -3,13 +3,7 @@
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
 
   nix.settings = {
-    substituters = [
-      "https://cache.garnix.io"
-      "https://attic.xuyh0120.win/lantian"
-    ];
-    trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-    ];
+    substituters = [ "https://attic.xuyh0120.win/lantian" ];
+    trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
   };
 }
