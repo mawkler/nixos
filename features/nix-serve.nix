768482d02b937a40d8cf2eaf6e879b401337e885
Melker Ulander <melker.ulander@pm.me>
2026-08-19 13:33:59 +0200

    Sign nix-serve cache with shared secret key

--- features/nix-serve.nix
+++ features/nix-serve.nix
@@ -5,10 +5,17 @@
     enable = true;
     openFirewall = true;
     package = pkgs.nix-serve-ng;
+    secretKeyFile = "/var/secrets/nix-serve/secret.key";
   };
 
   # Client
-  nix.settings.trusted-substituters = map (hostname: "http://${hostname}.local:5000") hostnames;
+  nix.settings = {
+    trusted-substituters = map (hostname: "http://${hostname}.local:5000") hostnames;
+    trusted-public-keys = [
+      "cache.thinkpad-nixos.local-1:GOD9LtryYSo83u53HDM7YxYRmsnrltvSFsOUlaqsWjo="
+    ];
+  };
+
   # So hosts can reach each other via `<hostname>.local`
   services.avahi = {
     enable = true;
