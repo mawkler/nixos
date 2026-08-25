8363e134af2b729288e7c71e1c1dfc5380f4caca
Melker Ulander <melker.ulander@pm.me>
2026-08-25 14:34:36 +0200

    nix: also add nix-serve machines to `substituters`

--- features/nix-serve.nix
+++ features/nix-serve.nix
@@ -1,25 +1,32 @@
 { pkgs, hostnames, ... }:
 {
   # Server
-  services.nix-serve = {
-    enable = true;
-    openFirewall = true;
-    package = pkgs.nix-serve-ng;
-    secretKeyFile = "/var/secrets/nix-serve/secret.key";
+  services = {
+    nix-serve = {
+      enable = true;
+      openFirewall = true;
+      package = pkgs.nix-serve-ng;
+      secretKeyFile = "/var/secrets/nix-serve/secret.key";
+    };
+
+    # So hosts can reach each other via `<hostname>.local`
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+    };
   };
 
   # Client
-  nix.settings = {
-    trusted-substituters = map (hostname: "http://${hostname}.local:5000") hostnames;
-    trusted-public-keys = [
-      "cache.thinkpad-nixos.local-1:GOD9LtryYSo83u53HDM7YxYRmsnrltvSFsOUlaqsWjo="
-      "cache.beauty.local-1:FRLDLnUQ+Shu5AzUenJ93K1NryLDFKvwHUOHmUEbtD8="
-    ];
-  };
-
-  # So hosts can reach each other via `<hostname>.local`
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-  };
+  nix.settings =
+    let
+      cacheUrls = map (hostname: "http://${hostname}.local:5000") hostnames;
+    in
+    {
+      substituters = cacheUrls;
+      trusted-substituters = cacheUrls;
+      trusted-public-keys = [
+        "cache.thinkpad-nixos.local-1:GOD9LtryYSo83u53HDM7YxYRmsnrltvSFsOUlaqsWjo="
+        "cache.beauty.local-1:FRLDLnUQ+Shu5AzUenJ93K1NryLDFKvwHUOHmUEbtD8="
+      ];
+    };
 }
