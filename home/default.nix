d075d43cef4dec8d3ceeb53534ab7347effbb24c
Melker Ulander <melker.ulander@pm.me>
2026-09-03 15:39:15 +0200

    mime-apps: add slack and claude-cli

--- home/default.nix
+++ home/default.nix
@@ -59,11 +59,15 @@ in
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "inode/directory" = "org.gnome.Nautilus.desktop";
+      # keep-sorted start
       "application/pdf" = "sioyek.desktop";
+      "inode/directory" = "org.gnome.Nautilus.desktop";
+      "text/plain" = "neovide.desktop";
+      "x-scheme-handler/claude-cli" = "claude-code-url-handler.desktop";
       "x-scheme-handler/http" = "brave-browser.desktop";
       "x-scheme-handler/https" = "brave-browser.desktop";
-      "text/plain" = "neovide.desktop";
+      "x-scheme-handler/slack" = "slack.desktop";
+      # keep-sorted end
     };
   };
 }
