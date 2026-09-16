41ecc7b315d4209d74d3a694117660c72003219f
Melker Ulander <melker.ulander@pm.me>
2026-09-16 14:40:54 +0200

    mimeapps: format

--- home/mimeapps.nix
+++ home/mimeapps.nix
@@ -7,11 +7,12 @@
       "application/pdf" = "sioyek.desktop";
       "inode/directory" = "org.gnome.Nautilus.desktop";
       "text/plain" = "neovide.desktop";
-      "x-scheme-handler/beeper"="beepertexts.desktop";
+      "x-scheme-handler/beeper" = "beepertexts.desktop";
       "x-scheme-handler/claude-cli" = "claude-code-url-handler.desktop";
       "x-scheme-handler/http" = "brave-browser.desktop";
       "x-scheme-handler/https" = "brave-browser.desktop";
       "x-scheme-handler/slack" = "slack.desktop";
       # keep-sorted end
     };
-  };}
+  };
+}
